4c82ff2d62e6674b7026c8ef4ecbf56567310aa6
silvitcornejo <silvi.cornejoo@gmail.com>
2024-11-15 16:52:27 +0100

    initial_commit_config-package.json

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight : 800,
+    viewportWidth : 1280,
+    baseUrl: 'https://lavaporeria.com/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
