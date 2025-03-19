dc9f3e4e66852e231cf4397c0c540ffdf0d83781
Silvit <silvi.cornejoo@gmail.com>
2025-03-19 09:38:24 +0100

    add assertions

--- cypress/e2e/product-vape.spec.cy.js
+++ cypress/e2e/product-vape.spec.cy.js
@@ -6,7 +6,9 @@ context('Lavaporeria Page Components Tests', () => {
 
     describe('Color options',()=>{
         it('Check Añadir al carrito status',()=>{
+          cy.get('.single_add_to_cart_button').should('have.class','disabled')
         cy.dropdownFilter('[class="value"] [id="pa_color"]','azul')
+        cy.get('.single_add_to_cart_button').should('not.have.class', 'disabled')
         })
     })
 })
